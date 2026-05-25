a6ffe4cd83eb23754ecb5ef40ca6235c057ff39f
Parth Surve <surveparth429@gmail.com>
2026-05-25 16:48:47 +0530

    feature: env validation and setup auth schema

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { Pool } from 'pg'
+import { env } from '@/lib/env'
 
 const globalPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
 const getPrismaClient = () => {
-  const connectionString = process.env.DATABASE_URL
+  const connectionString = env.DATABASE_URL
   const pool = new Pool({ connectionString })
   const adapter = new PrismaPg(pool)
   return new PrismaClient({
