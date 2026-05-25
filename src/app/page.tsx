9f27fa4fd9176f7b3b3730069bb04b105e5f1c69
Parth Surve <surveparth429@gmail.com>
2026-05-25 15:23:00 +0530

    feature: setup shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/ui/button'
+
 export default function HomePage() {
   return (
-    <main>
-      <h1>Blaze</h1>
+    <main className="flex min-h-screen items-center justify-center">
+      <Button>Blaze</Button>
     </main>
   )
 }
